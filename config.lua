7fba3c0ca77fa5a8c8051957740991c119b9392c
Felipe-Devr <felipegamerdev1@gmail.com>
2025-08-15 20:46:07 -0700

    Weebhook message test

---
+++ config.lua
@@ -0,0 +1,7 @@
+local function loadConfiguration() 
+	return {
+		weebhook = "https://discord.com/api/webhooks/1406120859193708544/Ww_ZBuBI9Fk4Ew6JqCT1HLZs-FuTdxQf8Rx8G2lI3fAne1BrBLgLJ5t4WWmCCr_-NbLb"
+	}
+end
+
+return loadConfiguration()
--- notifier.lua
+++ notifier.lua
@@ -1,1 +1,11 @@
-print("hello world")
+local internet = require("internet");
+
+local configuration = {
+	weebhook =
+	"https://discord.com/api/webhooks/1406120859193708544/Ww_ZBuBI9Fk4Ew6JqCT1HLZs-FuTdxQf8Rx8G2lI3fAne1BrBLgLJ5t4WWmCCr_-NbLb"
+}
+
+
+internet.request(configuration.weebhook, {
+	content = "Hello world from OpenComputers!"
+});
