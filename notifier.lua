d616230c4c9919012b9d1ef6608051072b77003b
Felipe-Devr <felipegamerdev1@gmail.com>
2025-08-15 21:05:34 -0700

    Test 1

--- notifier.lua
+++ notifier.lua
@@ -1,11 +1,33 @@
-local internet = require("internet");
+--- Libraries
 
+local internet = require("internet");
+local component = require("component");
+local controller = component.me_controller;
+
+
+--- Program Configuration
 local configuration = {
 	weebhook =
 	"https://discord.com/api/webhooks/1406120859193708544/Ww_ZBuBI9Fk4Ew6JqCT1HLZs-FuTdxQf8Rx8G2lI3fAne1BrBLgLJ5t4WWmCCr_-NbLb"
 }
 
 
-internet.request(configuration.weebhook, {
-	content = "Hello world from OpenComputers!"
-});
+local function notify(message)
+	internet.request(configuration.weebhook, {
+		content = message
+	});
+end
+
+local busyCpuCache = {};
+
+while true do
+	for cpuData in controller.getCpus() do
+		if cpuData.busy then
+			busyCpuCache[cpuData.name] = true;
+		end
+
+		if busyCpuCache[cpuData.name] and not cpuData.busy then  
+			notify("CPU " .. cpuData.name .. " finished")
+		end
+	end
+end
